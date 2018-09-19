1df56f1a7a82b61450dec917bd18255f164c457d
Dominika Tkaczyk <d.tkaczyk@gmail.com>
2018-09-19 11:51:13 +0000

    script for running a basic workflow

---
+++ scripts/run_basic.sh
@@ -0,0 +1,8 @@
+WORKING_DIR=$(mktemp -d)
+
+python3 dataset/draw_sample.py -s 100 -o $WORKING_DIR/sample.json
+python3 dataset/generate_dataset.py -l ieee -s $WORKING_DIR/sample.json -o $WORKING_DIR/dataset.json
+python3 matching/match_references.py -d $WORKING_DIR/dataset.json -o $WORKING_DIR/matched.json
+python3 evaluation/evaluate.py -d $WORKING_DIR/matched.json
+
+rm -r $WORKING_DIR
