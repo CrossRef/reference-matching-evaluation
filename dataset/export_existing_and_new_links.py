bdcdde4d7aed5b46f2b7ae8f5134e8ba92461730
Dominika Tkaczyk <d.tkaczyk@gmail.com>
2018-10-05 21:38:41 +0000

    sampling and inclusing null links

--- dataset/export_existing_and_new_links.py
+++ dataset/export_existing_and_new_links.py
@@ -5,6 +5,7 @@ import matching.stq_matcher
 import utils.data_format_keys as dfk
 
 from multiprocessing import Pool
+from random import sample
 from utils.utils import init_logging, read_json, save_json
 
 
@@ -15,7 +16,7 @@ def extract_unstructured_refs(sample_works):
         if dfk.CR_ITEM_REFERENCE not in work:
             continue
         for ref in work[dfk.CR_ITEM_REFERENCE]:
-            if ref.get(dfk.CR_ITEM_DOI_ASSERTED_BY) == 'crossref':
+            if ref.get(dfk.CR_ITEM_DOI_ASSERTED_BY) != 'publisher':
                 if ref.get(dfk.CR_ITEM_UNSTRUCTURED, False):
                     ref['source_doi'] = source_doi
                     references.append(ref)
@@ -28,6 +29,7 @@ if __name__ == '__main__':
     parser.add_argument('-v', '--verbose', help='verbose output',
                         action='store_true')
     parser.add_argument('-s', '--sample', type=str, required=True)
+    parser.add_argument('-n', '--references', type=int)
     parser.add_argument('-o', '--output', type=str, required=True)
 
     args = parser.parse_args()
@@ -37,6 +39,10 @@ if __name__ == '__main__':
     sample_data = read_json(args.sample)
 
     references = extract_unstructured_refs(sample_data)
+    logging.info('Number of references to analyze: {}'.format(len(references)))
+
+    if args.references is not None:
+        references = sample(references, args.references)
 
     logging.info('Number of references to analyze: {}'.format(len(references)))
 
@@ -46,13 +52,15 @@ if __name__ == '__main__':
                             [item['unstructured'] for item in references])
 
     matcher = matching.stq_matcher.Matcher()
-    with Pool() as p:
+    with Pool(3) as p:
         stq_results = p.map(matcher.match,
                             [item['unstructured'] for item in references])
 
     save_json([{'source_DOI': r['source_doi'].lower(),
                 'ref_string': r[dfk.CR_ITEM_UNSTRUCTURED],
-                'original_link': r[dfk.CR_ITEM_DOI].lower(),
+                'original_link': r[dfk.CR_ITEM_DOI].lower()
+                if r.get(dfk.CR_ITEM_DOI_ASSERTED_BY) == 'crossref'
+                else None,
                 'current_STQ_link': s[0] if s[0] is None else s[0].lower(),
                 'search_API_link': a[0] if a[0] is None else a[0].lower(),
                 'search_API_score': a[1]}
