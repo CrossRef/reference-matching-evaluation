bdcab4e7f1796dc107665a5306fe05d162872f5f
Dominika Tkaczyk <d.tkaczyk@gmail.com>
2018-10-07 15:07:18 +0000

    change default matcher

--- matching/match_config.py
+++ matching/match_config.py
@@ -1,15 +1,15 @@
 import matching.cr_search_simple_matcher
 import matching.stq_matcher
 
-from utils.utils import read_json
+# from utils.utils import read_json
 
 # STQ matcher
 # MATCHER = matching.stq_matcher.Matcher()
 
 # SA matcher without exclusion
-# MATCHER = matching.cr_search_simple_matcher.Matcher(-1)
+MATCHER = matching.cr_search_simple_matcher.Matcher(-1)
 
 # SA matcher with exclusion
-MATCHER = matching.cr_search_simple_matcher.Matcher(
-    -1, read_json('data/threshold/dataset-excluded-2500.json')
-    ['dataset_dois'])
+# MATCHER = matching.cr_search_simple_matcher.Matcher(
+#    -1, read_json('data/threshold/dataset-excluded-2500.json')
+#    ['dataset_dois'])
