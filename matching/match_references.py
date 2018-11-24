32126dfe025ff03f18a0bf569002ab28be5ebac1
Dominika Tkaczyk <d.tkaczyk@gmail.com>
2018-11-24 22:59:28 +0000

    more efficient processing

--- matching/match_references.py
+++ matching/match_references.py
@@ -9,6 +9,20 @@ from multiprocessing import Pool
 from utils.cr_utils import get_item
 from utils.utils import init_logging, keep_fields
 
+
+def transform(item):
+    fields = list(item.get(dfk.DATASET_TARGET_GT, {}).keys())
+    if fields == ['DOI']:
+        return item[dfk.DATASET_TARGET_TEST]
+    if item.get(dfk.DATASET_TARGET_TEST, {}).get(dfk.CR_ITEM_DOI) is None:
+        return item[dfk.DATASET_TARGET_TEST]
+    r_item = get_item(item.get(dfk.DATASET_TARGET_TEST, {})
+                      .get(dfk.CR_ITEM_DOI))
+    if r_item is not None:
+        return keep_fields(r_item, item.get(dfk.DATASET_TARGET_GT, {}).keys())
+    return item[dfk.DATASET_TARGET_TEST]
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='run a citation matcher')
     parser.add_argument('-v', '--verbose', help='verbose output',
@@ -33,13 +47,11 @@ if __name__ == '__main__':
                dfk.DATASET_MATCHER: MATCHER.description(),
                dfk.DATASET_SCORE: r[1]})
         for r, d in zip(results, dataset)]
-    for d in dataset:
-        if d.get(dfk.DATASET_TARGET_TEST, {}).get(dfk.CR_ITEM_DOI) is not None:
-            item = get_item(d.get(dfk.DATASET_TARGET_TEST, {})
-                            .get(dfk.CR_ITEM_DOI))
-            if item is not None:
-                item = keep_fields(item, d.get(dfk.DATASET_TARGET_GT, {}).keys())
-                d[dfk.DATASET_TARGET_TEST] = item
+
+    with Pool(config.THREADS) as p:
+        transformed = p.map(transform, dataset)
+    [d.update({dfk.DATASET_TARGET_TEST: t})
+        for t, d in zip(transformed, dataset)]
 
     dataset_data = {dfk.DATASET_DOIS: dataset_data.get(dfk.DATASET_DOIS),
                     dfk.DATASET_DATASET: dataset}
